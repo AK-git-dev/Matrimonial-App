9e044432127f89948958cce343cf3f72d40cbf73
Aashray Jain <aashrayjainkatni@gmail.com>
2021-05-27 11:52:33 +0530

    tabs added as component

---
+++ Frontend/matrimonial-app-ionic-angularjs/src/app/components/tabs/tabs.component.ts
@@ -0,0 +1,16 @@
+import { Component, NgModule, OnInit } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+@Component({
+  selector: 'app-tabs',
+  templateUrl: './tabs.component.html',
+  styleUrls: ['./tabs.component.scss'],
+})
+
+export class TabsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
--- Frontend/matrimonial-app-ionic-angularjs/src/app/home/home.module.ts
+++ Frontend/matrimonial-app-ionic-angularjs/src/app/home/home.module.ts
@@ -5,6 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
+import { TabsComponent } from '../components/tabs/tabs.component';
 
 
 @NgModule({
@@ -14,6 +15,6 @@ import { HomePageRoutingModule } from './home-routing.module';
     IonicModule,
     HomePageRoutingModule
   ],
-  declarations: [HomePage]
+  declarations: [HomePage, TabsComponent]
 })
 export class HomePageModule {}
