667090fc847dbd038f2f21a7c89ce71189a030dd
Aashray Jain <aashrayjainkatni@gmail.com>
2021-05-28 17:26:26 +0530

    done

--- Frontend/matrimonial-app-ionic-angularjs/src/app/app-routing.module.ts
+++ Frontend/matrimonial-app-ionic-angularjs/src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    loadChildren: () => import('./home/home.module').then(m => m.HomePageModule)
   },
   {
     path: '',
@@ -13,42 +13,43 @@ const routes: Routes = [
   },
   {
     path: 'login',
-    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+    loadChildren: () => import('./login/login.module').then(m => m.LoginPageModule)
   },
   {
     path: 'trust-score',
-    loadChildren: () => import('./trust-score/trust-score.module').then( m => m.TrustScorePageModule)
+    loadChildren: () => import('./trust-score/trust-score.module').then(m => m.TrustScorePageModule)
 
   },
   {
     path: 'user-home',
-    loadChildren: () => import('./user-home/user-home.module').then( m => m.UserHomePageModule)
-  },
-    path: 'notifications',
-    loadChildren: () => import('./notifications/notifications.module').then( m => m.NotificationsPageModule)
+    loadChildren: () => import('./user-home/user-home.module').then(m => m.UserHomePageModule)
   },
   {
-    path: 'registration',
-    loadChildren: () => import('./registration/registration.module').then( m => m.RegistrationPageModule)
+  path: 'notifications',
+  loadChildren: () => import('./notifications/notifications.module').then(m => m.NotificationsPageModule)
   },
-  {
-    path: 'pref-partner',
-    loadChildren: () => import('./pref-partner/pref-partner.module').then( m => m.PrefPartnerPageModule)
-  },
-  {
-    path: 'settings',
-    loadChildren: () => import('./settings/settings.module').then( m => m.SettingsPageModule)
-  },
+{
+  path: 'registration',
+    loadChildren: () => import('./registration/registration.module').then(m => m.RegistrationPageModule)
+},
+{
+  path: 'pref-partner',
+    loadChildren: () => import('./pref-partner/pref-partner.module').then(m => m.PrefPartnerPageModule)
+},
+{
+  path: 'settings',
+    loadChildren: () => import('./settings/settings.module').then(m => m.SettingsPageModule)
+},
 
 {
-    path: 'pref-part',
-    loadChildren: () => import('./pref-part/pref-part.module').then( m => m.PrefPartPageModule)
-  },
+  path: 'pref-part',
+    loadChildren: () => import('./pref-part/pref-part.module').then(m => m.PrefPartPageModule)
+},
 
-  {
-    path: 'ed-car',
-    loadChildren: () => import('./ed-car/ed-car.module').then( m => m.EdCarPageModule)
-  }
+{
+  path: 'ed-car',
+    loadChildren: () => import('./ed-car/ed-car.module').then(m => m.EdCarPageModule)
+}
   // {
   //   path: 'tab1',
   //   loadChildren: () => import('./tabs/tab1/tab1.module').then( m => m.Tab1PageModule)
@@ -61,8 +62,6 @@ const routes: Routes = [
   //   path: 'tabs2',
   //   loadChildren: () => import('./tabs2/tabs2.module').then( m => m.Tabs2PageModule)
   // },
-
-
 ];
 
 @NgModule({
