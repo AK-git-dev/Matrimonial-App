93aeafe44f02e5687c00fc9c8a1a266bb38d397f
sounishnath003 <flock.sinasini@gmail.com>
2021-05-30 14:14:48 +0530

    Set Limit atmost 10 DP + atmost 3 for Rel.Contacts

--- Backend/server/controllers/microservices/profile-picture.service.ts
+++ Backend/server/controllers/microservices/profile-picture.service.ts
@@ -1,96 +1,98 @@
-import { Router } from "express";
-import { requiresAuth } from "../../utils/middlewares/requires-auth.middleware";
+import {Router} from "express";
+import {requiresAuth} from "../../utils/middlewares/requires-auth.middleware";
 import {
-  FileUploadDirectoryPath,
-  FileUploadInterface,
-  Next,
-  RequestInterface,
-  ResponseInterface,
-  SUCCESS,
-  uuid,
+    FileUploadDirectoryPath ,
+    FileUploadInterface ,
+    Next ,
+    RequestInterface ,
+    ResponseInterface ,
+    SUCCESS ,
+    uuid ,
 } from "../../utils";
 import createHttpError from "http-errors";
 import path from "path";
-import { Schema } from "../../../database/schema";
+import {Schema} from "../../../database/schema";
 
-const router = Router();
+const router = Router ();
 
 async function handleFileUpload(
-  req: RequestInterface,
-  res: ResponseInterface,
-  next: Next
+    req: RequestInterface ,
+    res: ResponseInterface ,
+    next: Next
 ) {
-  try {
-    const UserId = (req as any).userId;
-    const files: FileUploadInterface[] = (req as any).files.files;
+    try {
+        const UserId = (req as any).userId;
+        const files: FileUploadInterface[] = (req as any).files.files as FileUploadInterface[];
 
-    if (!files || Object.keys(files).length === 0) {
-      throw new createHttpError.Forbidden("No files has been attached!");
+        const noOfPhotos: number = await Schema.ProfilPicture.count ({where :{UserId}});
+        if (noOfPhotos > 10) throw  new createHttpError.Forbidden (`you can not upload more than 10 profile pictures!`);
+
+        if (!files) {
+            throw new createHttpError.Forbidden ("No files has been attached!");
+        } else if (files.length < 2)
+            throw new createHttpError.Forbidden (
+                "Please select at least 2 photos of yours"
+            );
+
+
+        // Looping through various
+        // fileIterators and storing them - Multiple FileUploads
+        const uploadImages: any[] = [];
+
+        for (const file of files) {
+            const fileName: string = `${file.md5}${path.extname (file.name)}`;
+            const savePath: string = `${FileUploadDirectoryPath}/${fileName}`;
+            await file.mv (savePath);
+
+            uploadImages.push ({
+                id :uuid () ,
+                filename :`uploads/${fileName}` ,
+                UserId ,
+            });
+        }
+
+        await Schema.ProfilPicture.bulkCreate ([...uploadImages]);
+        next ();
+    } catch (e) {
+        next (e);
     }
-
-    if (files.length < 2)
-      throw new createHttpError.Forbidden(
-        "Please select at least 2 photos of yours"
-      );
-
-    // Looping through various
-    // fileIterators and storing them - Multiple FileUploads
-    const uploadImages: any[] = [];
-
-    for (const file of files) {
-      const fileName: string = `${file.md5}${path.extname(file.name)}`;
-      const savePath: string = `${FileUploadDirectoryPath}/${fileName}`;
-      await file.mv(savePath);
-
-      uploadImages.push({
-        id: uuid(),
-        filename: `uploads/${fileName}`,
-        UserId,
-      });
-    }
-
-    await Schema.ProfilPicture.bulkCreate([...uploadImages]);
-    next();
-  } catch (e) {
-    next(e);
-  }
 }
 
 // [POST] : ProfilePicture Upload Service
-router.post(
-  "/upload",
-  requiresAuth,
-  handleFileUpload,
-  async (req: RequestInterface, res: ResponseInterface, next: Next) => {
-    try {
-      return res.status(202).send({
-        ...SUCCESS,
-        message: "user profile picture has been updated!",
-      });
-    } catch (e) {
-      next(e);
+router.post (
+    "/upload" ,
+    requiresAuth ,
+    handleFileUpload ,
+    async (req: RequestInterface , res: ResponseInterface , next: Next) => {
+        try {
+            return res.status (202).send ({
+                ...SUCCESS ,
+                message :"user profile picture has been updated!" ,
+            });
+        } catch (e) {
+            next (e);
+        }
     }
-  }
 );
 
 // [DELETE]: Delete User Profile Picture
-router.delete(
-  "/delete/:pictureId",
-  requiresAuth,
-  async (req: RequestInterface, res: ResponseInterface, next: Next) => {
-    try {
-      const UserId = (req as any).userId;
-      const pictureId = (req as any).params.pictureId;
-      await Schema.ProfilPicture.destroy({ where: { id: pictureId } });
+router.delete (
+    "/delete/:pictureId" ,
+    requiresAuth ,
+    async (req: RequestInterface , res: ResponseInterface , next: Next) => {
+        try {
+            const UserId = (req as any).userId;
+            const pictureId = (req as any).params.pictureId;
+            await Schema.ProfilPicture.destroy ({where :{id :pictureId}});
 
-      return res.status(202).send({
-        ...SUCCESS,
-        message: "user image has been removed from profile picture!",
-      });
-    } catch (e) {
-      next(e);
+            return res.status (202).send ({
+                ...SUCCESS ,
+                message :"user image has been removed from profile picture!" ,
+            });
+        } catch (e) {
+            next (e);
+        }
     }
-  }
 );
 
 export default router;
