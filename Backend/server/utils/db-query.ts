cce9cacd1c4dca89105f3ea33ce8cc830b166b82
Sounish Nath <flock.sinasini@gmail.com>
2021-05-31 19:41:17 +0530

    Delete some files & notification-system-design added

--- Backend/server/utils/db-query.ts
+++ Backend/server/utils/db-query.ts
@@ -1,5 +1,22 @@
+import { FindOptions } from "sequelize";
 import { Schema } from "../../database/schema";
 
+// Do The Pagination on Profile Recommendation
+export const paginate = async (
+  query: any,
+  { page, pageSize }: { page: number; pageSize: number }
+): Promise<FindOptions<any>> => {
+  const offset = page * pageSize;
+  const limit = pageSize;
+
+  return {
+    ...query,
+    offset,
+    limit,
+  };
+};
+
+// To Get The Details of All User and Related Information
 export const getAllUsersWithAllDetails = async () =>
   await Schema.User.findAll({
     include: [
