d83fb56ad855ddb452a1af49412ef415183aaf97
Ankit-kadam24 <ankitkadam1997@gmail.com>
2021-05-31 20:43:07 +0530

    Settings and notifications update

--- Frontend/matrimonial-app-ionic-angularjs/src/app/components/tabs/tabs.component.ts
+++ Frontend/matrimonial-app-ionic-angularjs/src/app/components/tabs/tabs.component.ts
@@ -1,5 +1,7 @@
 import { Component, NgModule, OnInit } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MenuController, ModalController } from '@ionic/angular';
+import { NotificationsPage } from '../../notifications/notifications.page'
 
 @Component({
   selector: 'app-tabs',
@@ -9,8 +11,21 @@ import { RouterModule, Routes } from '@angular/router';
 
 export class TabsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private menu: MenuController, private modalController: ModalController) { }
 
   ngOnInit() {}
 
+  openFirst() {
+    this.menu.enable(true, 'first');
+    this.menu.open('first');
+  }
+
+  async presentModal() {
+    console.log('Inside modal')
+    const modal = await this.modalController.create({
+      component: NotificationsPage,
+      cssClass: 'my-custom-class'
+    });
+    return await modal.present();
+  }
 }
--- Frontend/matrimonial-app-ionic-angularjs/src/app/notifications/notifications.page.ts
+++ Frontend/matrimonial-app-ionic-angularjs/src/app/notifications/notifications.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
 
 @Component({
   selector: 'app-notifications',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NotificationsPage implements OnInit {
 
-  constructor() { }
+  constructor(private modalController: ModalController) { }
 
   ngOnInit() {
+
+  }
+  dismiss() {
+    console.log('Dismiss modal')
+    this.modalController.dismiss({
+      'dismissed': true
+    });
   }
 
 }
