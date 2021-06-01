a8c9955a169e6a482bf0e9b51cdc5cebd49ba2b4
sounishnath003 <flock.sinasini@gmail.com>
2021-06-01 13:12:03 +0530

    :pencil: Wrote Models for ResponseObjects

--- Backend/server/models/index.ts
+++ Backend/server/models/index.ts
@@ -125,3 +125,164 @@ export interface PrefferedPartnerInterface {
   expectedSalary: string;
   salaryType: string;
 }
+
+
+/*
+  ------------------DATAMODELS_SCHEMA_RESPONSES----------------------
+ */
+
+
+
+export interface User {
+  id?:                      string;
+  fullname?:                string;
+  dateOfBirth?:             Date;
+  age?:                     number;
+  gender?:                  string;
+  phoneNumber?:             string;
+  email?:                   string;
+  martialStatus?:           string;
+  motherTongue?:            string;
+  isCasteBarrier?:          boolean;
+  fathersName?:             string;
+  mothersName?:             string;
+  accountActive?:           boolean;
+  createdAt?:               Date;
+  updatedAt?:               Date;
+  Educations?:              Education[];
+  Address?:                 Address;
+  Caste?:                   Caste;
+  LifeStyle?:               LifeStyle;
+  UploadedDocument?:        null;
+  Occupation?:              Occupation;
+  PrefferedPartnerChoice?:  PrefferedPartnerChoice;
+  FamilyDetail?:            FamilyDetail;
+  ProfilePictures?:         FavouritePerson[];
+  RelativeContacts?:        RelativeContact[];
+  FavouritePersons?:        FavouritePerson[];
+  PersonWhoFavouritedYous?: FavouritePerson[];
+}
+
+export interface Address {
+  id?:             string;
+  address?:        string;
+  city?:           string;
+  district?:       string;
+  country?:        string;
+  zipCode?:        string;
+  residentStatus?: null;
+  createdAt?:      Date;
+  updatedAt?:      Date;
+  UserId?:         string;
+}
+
+export interface Caste {
+  id?:        string;
+  caste?:     string;
+  subCaste?:  string;
+  createdAt?: Date;
+  updatedAt?: Date;
+  UserId?:    string;
+}
+
+export interface Education {
+  id?:               string;
+  type?:             string;
+  degree?:           string;
+  institutionName?:  string;
+  specializationIn?: string;
+  passoutYear?:      string;
+  createdAt?:        Date;
+  updatedAt?:        Date;
+  UserId?:           string;
+}
+
+export interface FamilyDetail {
+  id?:               string;
+  fatherName?:       string;
+  fatherOccupation?: string;
+  motherName?:       string;
+  motherOccupation?: string;
+  noOfBrothers?:     number;
+  noOfSisters?:      number;
+  familyStatus?:     string;
+  familyValues?:     string;
+  ancestralOrigin?:  string;
+  createdAt?:        Date;
+  updatedAt?:        Date;
+  UserId?:           string;
+}
+
+export interface FavouritePerson {
+  id?:                      string;
+  favouritePersonId?:       string;
+  createdAt?:               Date;
+  updatedAt?:               Date;
+  UserId?:                  string;
+  personWhoFavoritedYouID?: string;
+  filename?:                string;
+}
+
+export interface LifeStyle {
+  id?:                     string;
+  height?:                 string;
+  weight?:                 number;
+  bloodGroup?:             string;
+  dressStyle?:             string;
+  bodyShape?:              string;
+  skinComplextion?:        string;
+  diet?:                   string;
+  drinkingHabits?:         null;
+  smokingHabits?:          string;
+  sportsFitness?:          string;
+  anyChildren?:            null;
+  dateOfMarriage?:         null;
+  dateDivorced?:           null;
+  isDivorced?:             null;
+  reasonForDivorced?:      null;
+  haveAnyDieases?:         boolean;
+  descriptionOfDieseases?: string;
+  createdAt?:              Date;
+  updatedAt?:              Date;
+  UserId?:                 string;
+  PrivacySettingId?:       null;
+  favouriteMovies?:        any[];
+  honeymoonPlaces?:        any[];
+  languagesCanSpeak?:      any[];
+}
+
+export interface Occupation {
+  id?:                 string;
+  type?:               string;
+  currentCompanyName?: string;
+  salary?:             number;
+  isSelfEmployeed?:    boolean;
+  createdAt?:          Date;
+  updatedAt?:          Date;
+  UserId?:             string;
+}
+
+export interface PrefferedPartnerChoice {
+  id?:             string;
+  minHeight?:      string;
+  maxHeight?:      string;
+  minAge?:         string;
+  maxAge?:         string;
+  expectedSalary?: string;
+  salaryType?:     string;
+  createdAt?:      Date;
+  updatedAt?:      Date;
+  UserId?:         string;
+}
+
+export interface RelativeContact {
+  id?:          string;
+  type?:        string;
+  fullname?:    string;
+  phoneNumber?: string;
+  UserId?:      string;
+  createdAt?:   Date;
+  updatedAt?:   Date;
+}
+
+
