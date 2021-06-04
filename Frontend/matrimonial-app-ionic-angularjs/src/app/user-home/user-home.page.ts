171f201abdd66609a4bcec2e8a3fa4db7d15fd25
Aashray Jain <aashrayjainkatni@gmail.com>
2021-06-04 18:57:55 +0530

    infinite scroll added in user-home

--- Frontend/matrimonial-app-ionic-angularjs/src/app/user-home/user-home.page.ts
+++ Frontend/matrimonial-app-ionic-angularjs/src/app/user-home/user-home.page.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonInfiniteScroll } from '@ionic/angular';
 
 @Component({
   selector: 'app-user-home',
@@ -9,9 +10,25 @@ export class UserHomePage implements OnInit {
 
   username: string = 'Aashray Jain'
 
+  @ViewChild(IonInfiniteScroll)
+  infiniteScroll: IonInfiniteScroll;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  loadData(event) {
+    setTimeout(() => {
+      console.log('Done');
+      event.target.complete();
+
+      //logic
+      let data = [1, 2, 3]
+      if (data.length == 2) {
+        event.target.disabled = true;
+      }
+    }, 10000);
   }
 
 }
