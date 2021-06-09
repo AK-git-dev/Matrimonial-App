9cac26c6fe4572367626173f123ea0f58b328b94
sounishnath003 <flock.sinasini@gmail.com>
2021-06-09 13:32:25 +0530

    :ok_hand: Now Update Documents with ease

--- Backend/server/controllers/microservices/document.service.ts
+++ Backend/server/controllers/microservices/document.service.ts
@@ -16,56 +16,53 @@ const router = Router();
 
 // * Utility Function to upload Files
 
-async function handleFileUpload(
-  fileType: string,
-  req: RequestInterface,
-  next: Next
-) {
-  try {
-    // get the UserId first
-    const UserId: string = (req as any).userId;
+function handleFileUpload(fileType: string) {
+  return async function (
+    req: RequestInterface,
+    res: ResponseInterface,
+    next: Next
+  ) {
+    try {
+      // get the UserId first
+      const UserId: string = (req as any).userId;
 
-    // grab the file from req.files
-    const file: FileUploadInterface = (req.files as any)
-      .file as FileUploadInterface;
+      // grab the file from req.files
+      const file: FileUploadInterface = (req.files as any)
+        .file as FileUploadInterface;
 
-    const filename: string = `${file.md5}${path.extname(file.name)}`;
-    const savePath: string = `${FileUploadDirectoryPath}/${filename}`;
-    await file
-      .mv(savePath)
-      .catch(
-        () =>
-          new createError.Forbidden(
-            "Something went wrong! File cannot be saved! Please try again!"
-          )
+      const filename: string = `${file.md5}${path.extname(file.name)}`;
+      const savePath: string = `${FileUploadDirectoryPath}/${filename}`;
+      await file
+        .mv(savePath)
+        .catch(
+          () =>
+            new createError.Forbidden(
+              "Something went wrong! File cannot be saved! Please try again!"
+            )
+        );
+
+     await Schema.UploadedDocument.findOrCreate({ where: { UserId } });
+
+      // update
+      await Schema.UploadedDocument.update(
+        { [fileType]: `uploads/${filename}` },
+        { where: { UserId } }
       );
 
-    await Schema.UploadedDocument.findOrCreate({ where: { UserId } });
-
-    // update
-    await Schema.UploadedDocument.update(
-      { [fileType]: savePath },
-      { where: { UserId } }
-    );
-    return next();
-  } catch (error) {
-    next(error);
-  }
+      next();
+    } catch (error) {
+      next(error);
+    }
+  };
 }
 
 // [POST]: Aadhar Card Upload
 router.post(
   "/upload-aadhar",
   requiresAuth,
+  handleFileUpload("aadharCard"),
   async (req: RequestInterface, res: ResponseInterface, next: Next) => {
     try {
-      await handleFileUpload("aadharCard", req, next).catch(
-        () =>
-          new createError.Forbidden(
-            "Aadhar card uploading failed! Please try again!"
-          )
-      );
-
       return res.status(202).send({
         ...SUCCESS,
         message: "Aadhar card has been uploaded succesfully!",
@@ -80,15 +77,9 @@ router.post(
 router.post(
   "/upload-driving-license",
   requiresAuth,
+  handleFileUpload("drivingLicense"),
   async (req: RequestInterface, res: ResponseInterface, next: Next) => {
     try {
-      await handleFileUpload("drivingLicense", req, next).catch(
-        () =>
-          new createError.Forbidden(
-            "Driving License uploading failed! Please try again!"
-          )
-      );
-
       return res.status(202).send({
         ...SUCCESS,
         message: "Driving License has been uploaded succesfully!",
@@ -103,15 +94,9 @@ router.post(
 router.post(
   "/upload-passport",
   requiresAuth,
+  handleFileUpload("passport"),
   async (req: RequestInterface, res: ResponseInterface, next: Next) => {
     try {
-      await handleFileUpload("passport", req, next).catch(
-        () =>
-          new createError.Forbidden(
-            "Passport uploading failed! Please try again!"
-          )
-      );
-
       return res.status(202).send({
         ...SUCCESS,
         message: "Passport has been uploaded succesfully!",
@@ -126,15 +111,9 @@ router.post(
 router.post(
   "/upload-voter-card",
   requiresAuth,
+  handleFileUpload("voterCard"),
   async (req: RequestInterface, res: ResponseInterface, next: Next) => {
     try {
-      await handleFileUpload("voterCard", req, next).catch(
-        () =>
-          new createError.Forbidden(
-            "Voter Card uploading failed! Please try again!"
-          )
-      );
-
       return res.status(202).send({
         ...SUCCESS,
         message: "Voter Card has been uploaded succesfully!",
@@ -149,15 +128,9 @@ router.post(
 router.post(
   "/upload-pan-card",
   requiresAuth,
+  handleFileUpload("panCard"),
   async (req: RequestInterface, res: ResponseInterface, next: Next) => {
     try {
-      await handleFileUpload("panCard", req, next).catch(
-        () =>
-          new createError.Forbidden(
-            "Pan Card uploading failed! Please try again!"
-          )
-      );
-
       return res.status(202).send({
         ...SUCCESS,
         message: "Pan Card has been uploaded succesfully!",
@@ -172,15 +145,9 @@ router.post(
 router.post(
   "/upload-graduate-certificate",
   requiresAuth,
+  handleFileUpload("graduateCertificate"),
   async (req: RequestInterface, res: ResponseInterface, next: Next) => {
     try {
-      await handleFileUpload("graduateCertificate", req, next).catch(
-        () =>
-          new createError.Forbidden(
-            "Graduation Certificate uploading failed! Please try again!"
-          )
-      );
-
       return res.status(202).send({
         ...SUCCESS,
         message: "Graduation Certificate has been uploaded succesfully!",
@@ -195,15 +162,9 @@ router.post(
 router.post(
   "/upload-hs-certificate",
   requiresAuth,
+  handleFileUpload("higherSecondaryCertificate"),
   async (req: RequestInterface, res: ResponseInterface, next: Next) => {
     try {
-      await handleFileUpload("higherSecondaryCertificate", req, next).catch(
-        () =>
-          new createError.Forbidden(
-            "HS Certificate uploading failed! Please try again!"
-          )
-      );
-
       return res.status(202).send({
         ...SUCCESS,
         message: "HS Certificate has been uploaded succesfully!",
@@ -218,15 +179,9 @@ router.post(
 router.post(
   "/upload-secondary-certificate",
   requiresAuth,
+  handleFileUpload("secondaryCertificate"),
   async (req: RequestInterface, res: ResponseInterface, next: Next) => {
     try {
-      await handleFileUpload("secondaryCertificate", req, next).catch(
-        () =>
-          new createError.Forbidden(
-            "Secondary Certificate uploading failed! Please try again!"
-          )
-      );
-
       return res.status(202).send({
         ...SUCCESS,
         message: "Secondary Certificate has been uploaded succesfully!",
@@ -241,15 +196,9 @@ router.post(
 router.post(
   "/upload-diplome-certificate",
   requiresAuth,
+  handleFileUpload("diplomaCertificate"),
   async (req: RequestInterface, res: ResponseInterface, next: Next) => {
     try {
-      await handleFileUpload("diplomaCertificate", req, next).catch(
-        () =>
-          new createError.Forbidden(
-            "Diploma Certificate uploading failed! Please try again!"
-          )
-      );
-
       return res.status(202).send({
         ...SUCCESS,
         message: "Diploma Certificate has been uploaded succesfully!",
@@ -264,15 +213,9 @@ router.post(
 router.post(
   "/upload-appointment-letter",
   requiresAuth,
+  handleFileUpload("appointmentLetter"),
   async (req: RequestInterface, res: ResponseInterface, next: Next) => {
     try {
-      await handleFileUpload("appointmentLetter", req, next).catch(
-        () =>
-          new createError.Forbidden(
-            "Appointment Letter uploading failed! Please try again!"
-          )
-      );
-
       return res.status(202).send({
         ...SUCCESS,
         message: "Appointment Letter has been uploaded succesfully!",
@@ -287,15 +230,9 @@ router.post(
 router.post(
   "/upload-trade-license",
   requiresAuth,
+  handleFileUpload("tradeLicense"),
   async (req: RequestInterface, res: ResponseInterface, next: Next) => {
     try {
-      await handleFileUpload("tradeLicense", req, next).catch(
-        () =>
-          new createError.Forbidden(
-            "Trade License uploading failed! Please try again!"
-          )
-      );
-
       return res.status(202).send({
         ...SUCCESS,
         message: "Trade License has been uploaded succesfully!",
@@ -310,15 +247,9 @@ router.post(
 router.post(
   "/upload-taxation",
   requiresAuth,
+  handleFileUpload("taxation"),
   async (req: RequestInterface, res: ResponseInterface, next: Next) => {
     try {
-      await handleFileUpload("taxation", req, next).catch(
-        () =>
-          new createError.Forbidden(
-            "Taxation uploading failed! Please try again!"
-          )
-      );
-
       return res.status(202).send({
         ...SUCCESS,
         message: "Taxation has been uploaded succesfully!",
@@ -333,15 +264,9 @@ router.post(
 router.post(
   "/upload-shop-aggrement",
   requiresAuth,
+  handleFileUpload("shopAggrement"),
   async (req: RequestInterface, res: ResponseInterface, next: Next) => {
     try {
-      await handleFileUpload("shopAggrement", req, next).catch(
-        () =>
-          new createError.Forbidden(
-            "Shop Aggrement uploading failed! Please try again!"
-          )
-      );
-
       return res.status(202).send({
         ...SUCCESS,
         message: "Shop Aggrement has been uploaded succesfully!",
@@ -356,15 +281,9 @@ router.post(
 router.post(
   "/upload-bank-passbook",
   requiresAuth,
+  handleFileUpload("bankPassbook"),
   async (req: RequestInterface, res: ResponseInterface, next: Next) => {
     try {
-      await handleFileUpload("bankPassbook", req, next).catch(
-        () =>
-          new createError.Forbidden(
-            "Bank Passbook uploading failed! Please try again!"
-          )
-      );
-
       return res.status(202).send({
         ...SUCCESS,
         message: "Bank Passbook has been uploaded succesfully!",
@@ -379,15 +298,9 @@ router.post(
 router.post(
   "/upload-salary-slip",
   requiresAuth,
+  handleFileUpload("salarySlip"),
   async (req: RequestInterface, res: ResponseInterface, next: Next) => {
     try {
-      await handleFileUpload("salarySlip", req, next).catch(
-        () =>
-          new createError.Forbidden(
-            "Salary Slip uploading failed! Please try again!"
-          )
-      );
-
       return res.status(202).send({
         ...SUCCESS,
         message: "Salary Slip has been uploaded succesfully!",
