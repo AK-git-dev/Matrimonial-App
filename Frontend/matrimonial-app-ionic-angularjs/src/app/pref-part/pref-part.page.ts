e41de7cbab954ffff1db9d2665a7dfb743dfeae4
Aashray Jain <aashrayjainkatni@gmail.com>
2021-06-09 17:49:28 +0530

    pref-part

--- Frontend/matrimonial-app-ionic-angularjs/src/app/pref-part/pref-part.page.ts
+++ Frontend/matrimonial-app-ionic-angularjs/src/app/pref-part/pref-part.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { PickerController } from '@ionic/angular';
 
 @Component({
   selector: 'app-pref-part',
@@ -8,13 +9,71 @@ import { Router } from '@angular/router';
 })
 export class PrefPartPage implements OnInit {
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private pickerController: PickerController) { }
+
+  // multiColumnOptions = [
+  //   [
+  //     "4'8", "4'9", "5'0", "5'1", "5'2", "5'3", "5'4", "5'5", "5'6", "5'7", "5'8", "5'9", "6'0", 
+  //     "6'1", "6'2", "6'3", "6'4", "6'5", "6'6", "6'7"
+  //   ], 
+  //   [
+  //     "4'8", "4'9", "5'0", "5'1", "5'2", "5'3", "5'4", "5'5", "5'6", "5'7", "5'8", "5'9", "6'0", 
+  //     "6'1", "6'2", "6'3", "6'4", "6'5", "6'6", "6'7"
+  //   ]
+  // ]
+  
 
   ngOnInit() {
   }
 
-  onClick() {
-    this.router.navigate(['/user-home'])
+  // onClick() {
+  //   this.router.navigate(['/user-home'])
+  // }
+
+  submit() {
+    this.router.navigate(['/user-home']);
   }
+
+  // async openPicker(numColums, numOptions, columnOptions) {
+  //   const picker = await this.pickerController.create({
+  //     columns: this.getColumns(numColums, numOptions, columnOptions),
+  //     buttons: [
+  //       {
+  //         text: 'Cancel', 
+  //         role: 'cancel'
+  //       }, 
+  //       {
+  //         text: 'Confirm', 
+  //         handler: (value) => {
+  //           console.log(value);
+  //         }
+  //       }
+  //     ]
+  //   });
+
+  //   await picker.present();
+  // }
+
+  // getColumns(numColums, numOptions, columnOptions) {
+  //   let columns = []
+  //   for (let i = 0; i < numColums; ++i) {
+  //     columns.push({
+  //       name: `col-${i}`,
+  //       options: this.getColumnOptions(i, numOptions, columnOptions)
+  //     });
+  //   }
+  //   return columns;
+  // }
+
+  // getColumnOptions(columnIndex, numOptions, columnOptions) {
+  //   let options = [];
+  //   for (let i = 0; i < numOptions; ++i) {
+  //     options.push({
+  //       text: columnOptions[columnIndex][i % numOptions],
+  //       value: i
+  //     });
+  //   }
+  //   return options;
+  // }
 
 }
