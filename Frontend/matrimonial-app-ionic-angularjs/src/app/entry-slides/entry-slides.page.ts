4c295d4458adc330bc0acbd5df6972e21ea9235d
Ankit-kadam24 <ankitkadam1997@gmail.com>
2021-06-14 16:12:45 +0530

    Commit 207

--- Frontend/matrimonial-app-ionic-angularjs/src/app/entry-slides/entry-slides.page.ts
+++ Frontend/matrimonial-app-ionic-angularjs/src/app/entry-slides/entry-slides.page.ts
@@ -7,6 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EntrySlidesPage implements OnInit {
 
+  slideOpts = {
+    initialSlide: 0,
+    speed: 400
+  };
+
+
+
+  move(slides){
+    // console.log(slides)
+    slides.slideTo(1)
+}
+move1(slides){
+  // console.log(slides)
+  slides.slideTo(2)
+}
+
+
   constructor() { }
 
   ngOnInit() {
