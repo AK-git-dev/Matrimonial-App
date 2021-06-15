41c14287c2565270942317f6438e53a0c8ecb177
sounishnath003 <flock.sinasini@gmail.com>
2021-06-15 17:09:57 +0530

    Mutual matches done!

--- Backend/server/controllers/api.controller.ts
+++ Backend/server/controllers/api.controller.ts
@@ -16,6 +16,7 @@ import UserFavoriteController from "./add-to-favourites.controller";
 import NotificationController from "./notification.controller";
 import JustJoinersController from './just-joinee.controller';
 import RequestController from './request-controller';
+import MutualMatchController from './mutualConnection.controller';
 
 const router = Router();
 
@@ -51,6 +52,7 @@ router.use("/profiles-recommendations", ProfileRecommendationController);
 router.use("/users", UserController);
 router.use('/just-joiners', JustJoinersController);
 router.use('/request', RequestController);
+router.use('/mutual-matches', MutualMatchController);
 router.use("/upload-service", FileUploaderController);
 router.use("/user/favorites/0", UserFavoriteController);
 router.use("/notifications", NotificationController);
